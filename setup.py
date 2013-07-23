29cfb92e32ec77fef19873498aa3b5221b92c35c
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-07-23 16:16:22 +0300

    Initial commit of the setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='vmconnector',
+      version='0.1.0',
+      description='VMware Connector module',
+      author='Marin Atanasov Nikolov',
+      author_email='dnaeon@gmail.com',
+      license='BSD',
+      packages=['vmconnector'],
+      package_dir={'vmconnector': 'src/vmconnector'}
+)
