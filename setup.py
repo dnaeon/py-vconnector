640c3b2eb3dcd7a87ee8e5dd57d6a12fa51a1d28
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-09-13 17:23:41 +0300

    vconnector version 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vconnector',
-      version='0.2.9',
+      version='0.3.0',
       description='VMware vSphere Connector Module for Python',
       author='Marin Atanasov Nikolov',
       author_email='dnaeon@gmail.com',
