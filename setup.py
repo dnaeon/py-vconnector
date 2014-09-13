b11d14f14fc23d6faa65375fffd2deb46763cac0
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-09-13 17:44:41 +0300

    Revert "Add long_description to setup.py"
    This reverts commit e7e23b62ae2a06567bf1f4b50bad5babae702787.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='vconnector',
       version='0.3.0',
       description='VMware vSphere Connector Module for Python',
-      long_description=read('README.md')
       author='Marin Atanasov Nikolov',
       author_email='dnaeon@gmail.com',
       license='BSD',
