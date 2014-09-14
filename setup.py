73ecf64567d06006de755a18f5b8c5de0aabf96c
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-09-14 10:28:42 +0300

    Add long_description and download_url to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(name='vconnector',
       version='0.3.0',
       description='VMware vSphere Connector Module for Python',
+      long_description=open('README.rst').read(),
       author='Marin Atanasov Nikolov',
       author_email='dnaeon@gmail.com',
       license='BSD',
       url='https://github.com/dnaeon/py-vconnector',
+      download_url='https://github.com/dnaeon/py-vconnector/releases',
       packages=['vconnector'],
       package_dir={'': 'src'},
       scripts=[
